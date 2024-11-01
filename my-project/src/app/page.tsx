53ec74cbeedb86b2757d5fe1b988af94b521965f
Aaron Varga <aaronvarga@myyahoo.com>
2024-11-01 22:51:38 +0000

    css updates

--- my-project/src/app/page.tsx
+++ my-project/src/app/page.tsx
@@ -17,15 +17,15 @@ export default function Home() {
       </header>
       <main className="p-4 bg-stone-900 rounded-xl border border-stone-800 container mx-auto">
         <section className="flex flex-col gap-4">
-          <div className="grid grid-rows-2 grid-flow-col gap-4 sm:grid-rows-1">
+          <div className="grid grid-rows-2 grid-flow-col gap-4 sm:grid-rows-1 sm:pr-2">
             <button className="flex items-center justify-center border border-stone-800 p-4 rounded-xl aspect-square">01</button>
             <button className="flex items-center justify-center border border-stone-800 p-4 rounded-xl aspect-square">02</button>
             <button className="flex items-center justify-center border border-stone-800 p-4 rounded-xl aspect-square">03</button>
             <button className="flex items-center justify-center border border-stone-800 p-4 rounded-xl aspect-square">04</button>
           </div>
-          <h1 className="font-bold text-xl text-stone-200">Welcome to my app!</h1>
-          <p className="text-stone-400">This is an app idea I had in mind I wanted to make for a while and finally got around to it recently.</p>
-          <p className="text-stone-400">There will be many rapid iterations ongoing likely filled with many updates. My goal is to have this functioning ASAP.</p>
+          <h1 className="font-bold text-xl text-stone-200 px-4">Welcome to my app!</h1>
+          <p className="text-stone-400 px-4">This is an app idea I had in mind I wanted to make for a while and finally got around to it recently.</p>
+          <p className="text-stone-400 px-4">There will be many rapid iterations ongoing likely filled with many updates. My goal is to have this functioning ASAP.</p>
         </section>
       </main>
     </div>
